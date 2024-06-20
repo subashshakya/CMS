d07510b97c444551c3e6ab995f191644a7d71cf3
subashshakya <subashshakya12@gmail.com>
2024-06-20 23:10:28 +0545

    feat: add util function to update fields

--- cms/web/utils/util.py
+++ cms/web/utils/util.py
@@ -5,3 +5,7 @@ def find_missing_fields(
     required_fields: List[str], request_fields: List[str]
 ) -> List[str]:
     return [field for field in required_fields if field not in request_fields]
+
+
+def update_fields(db_obj: object, request_obj: object) -> object:
+    return {key: request_obj[key] for key in db_obj.keys()}
